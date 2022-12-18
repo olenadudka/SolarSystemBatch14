d25b1e0927bbf06f3cef09848c68ee9a2a9c4a1c
olenadudka <helen.lady2311@gmail.com>
2022-12-18 11:20:11 -0500

    The basic project Solar System is ready for the review

---
+++ src/SolarSystem.java
@@ -0,0 +1,12 @@
+public class SolarSystem {
+    int Sun;
+    int Moon;
+    int Planets;
+
+    public static void main(String[] args) {
+        SolarSystem Jinny=new SolarSystem();
+        Jinny.Sun=1;
+        Jinny.Moon=2;
+        Jinny.Planets=3;
+    }
+}
