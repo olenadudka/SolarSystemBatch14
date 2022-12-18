af202026c9e813d4df2a1d8d5f30c85d9f38359e
olenadudka <helen.lady2311@gmail.com>
2022-12-18 10:22:20 -0500

    The basic structure of the Solar sysytem

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("The basic structure is here please write down your code in this "+"project");
+    }
+}
